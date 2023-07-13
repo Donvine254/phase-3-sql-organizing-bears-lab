1656ffb178f489bafde3ac58d5a6f6e41f7b59fe
donvine mugendi <donvinemugendi@gmail.com>
2023-07-13 16:01:42 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  ('Tabitha', 2, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Assertive', 1),
+  ('Melissa', 4, 'F', 'Golden', 'Playful', 1),
+  ('Grinch', 1, 'M', 'Green', 'Mischievous', 1),
+  ('Wendy', 2, 'F', 'White', 'Gentle', 1),
+  (NULL, 5, 'M', 'White', 'playful', 0);
